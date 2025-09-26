410e36f329008d7936c03f320ab7bb2e391b4ce5
zz <1416333788@qq.com>
2025-09-26 17:51:21 +0800

    second

--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -36,8 +36,9 @@ public class UserController {
      */
     @PostMapping("code")
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
-        // TODO 发送短信验证码并保存验证码
-        return Result.fail("功能未完成");
+        // 发送短信验证码并保存验证码
+        userService.sendCode(phone, session);
+        return Result.ok();
     }
 
     /**
@@ -46,8 +47,9 @@ public class UserController {
      */
     @PostMapping("/login")
     public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session){
-        // TODO 实现登录功能
-        return Result.fail("功能未完成");
+        //  实现登录功能
+        userService.login(loginForm, session);
+        return Result.ok();
     }
 
     /**
